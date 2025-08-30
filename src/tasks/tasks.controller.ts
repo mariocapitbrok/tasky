13f9401aa524dbbc9a38d21abbe1a4fe9d95add9
mariocapitbrok <mario@capitbrok.com>
2025-08-30 12:59:17 -0600

    Create Tasks Service and Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasks: TasksService) {}
+
+  @Get()
+  list(@Req() req: any) {
+    return this.tasks.list(req.user.userId);
+  }
+
+  @Post()
+  create(@Req() req: any, @Body() dto: CreateTaskDto) {
+    return this.tasks.create(req.user.userId, dto);
+  }
+
+  @Patch(':id')
+  update(@Req() req: any, @Param('id') id: string, @Body() dto: UpdateTaskDto) {
+    return this.tasks.update(req.user.userId, Number(id), dto);
+  }
+
+  @Delete(':id')
+  remove(@Req() req: any, @Param('id') id: string) {
+    return this.tasks.remove(req.user.id, Number(id));
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,8 +7,24 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 export class TasksService {
   constructor(private prisma: PrismaService) {}
 
-  list(userId: number) {}
-  create(userId: number, dto: CreateTaskDto) {}
-  update(userId: number, Id: number, dto: UpdateTaskDto) {}
-  remove(userId: number, id: number) {}
+  list(userId: number) {
+    return this.prisma.task.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+  create(userId: number, dto: CreateTaskDto) {
+    return this.prisma.task.create({
+      data: { ...dto, userId },
+    });
+  }
+  update(userId: number, id: number, dto: UpdateTaskDto) {
+    return this.prisma.task.update({
+      where: { id },
+      data: dto,
+    });
+  }
+  remove(userId: number, id: number) {
+    return this.prisma.task.delete({ where: { id } });
+  }
 }
