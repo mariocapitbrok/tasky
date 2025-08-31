252342adf7f26297c304256c65f73b7b49d3d1d3
mariocapitbrok <mario@capitbrok.com>
2025-08-30 23:32:58 -0600

    Fix lints at Tasks Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -13,6 +13,13 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Request } from 'express';
+
+interface RequestWithUser extends Request {
+  user: {
+    userId: number;
+  };
+}
 
 @Controller('tasks')
 @UseGuards(JwtAuthGuard)
@@ -20,22 +27,26 @@ export class TasksController {
   constructor(private tasks: TasksService) {}
 
   @Get()
-  list(@Req() req: any) {
+  list(@Req() req: RequestWithUser) {
     return this.tasks.list(req.user.userId);
   }
 
   @Post()
-  create(@Req() req: any, @Body() dto: CreateTaskDto) {
+  create(@Req() req: RequestWithUser, @Body() dto: CreateTaskDto) {
     return this.tasks.create(req.user.userId, dto);
   }
 
   @Patch(':id')
-  update(@Req() req: any, @Param('id') id: string, @Body() dto: UpdateTaskDto) {
+  update(
+    @Req() req: RequestWithUser,
+    @Param('id') id: string,
+    @Body() dto: UpdateTaskDto,
+  ) {
     return this.tasks.update(req.user.userId, Number(id), dto);
   }
 
   @Delete(':id')
-  remove(@Req() req: any, @Param('id') id: string) {
+  remove(@Req() req: RequestWithUser, @Param('id') id: string) {
     return this.tasks.remove(req.user.userId, Number(id));
   }
 }
